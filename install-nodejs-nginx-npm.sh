c3061b227189827976942d4a9113b8fb3a27c63a
davidglaeseman <backgroundcolor@gmail.com>
2017-08-25 11:32:03 -0700

    Create install-nodejs-nginx-npm.sh

---
+++ install-nodejs-nginx-npm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Since Ubuntu comes with Node JS 4.6, we should upgrade to 6+
+sudo apt-get update
+sudo apt-get upgrade -y
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs

84deade7ba2dcf1e744bef6d0b27ae124a074258
davidglaeseman <backgroundcolor@gmail.com>
2017-08-25 11:30:06 -0700

    Update and rename globals.sh to install-php-apache-mysql.sh

---
+++ install-php-apache-mysql.sh
@@ -0,0 +1,41 @@
+# Nothing Here :(
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update && sudo apt-get install php7.1
+sudo apt-get install -y php7.1-xml
+sudo apt-get install -y php7.1-xsl
+sudo apt-get install -y php7.1-mbstring
+sudo apt-get install -y php7.1-readline
+sudo apt-get install -y php7.1-zip
+sudo apt-get install -y php7.1-mysql
+sudo apt-get install -y php7.1-phpdbg
+sudo apt-get install -y php7.1-interbase
+sudo apt-get install -y php7.1-sybase
+sudo apt-get install -y php7.1-sqlite3
+sudo apt-get install -y php7.1-tidy
+sudo apt-get install -y php7.1-opcache
+sudo apt-get install -y php7.1-pspell
+sudo apt-get install -y php7.1-json
+sudo apt-get install -y php7.1-xmlrpc
+sudo apt-get install -y php7.1-curl
+sudo apt-get install -y php7.1-ldap
+sudo apt-get install -y php7.1-bz2
+sudo apt-get install -y php7.1-cgi
+sudo apt-get install -y php7.1-imap
+sudo apt-get install -y php7.1-cli
+sudo apt-get install -y php7.1-dba
+sudo apt-get install -y php7.1-dev
+sudo apt-get install -y php7.1-intl
+sudo apt-get install -y php7.1-fpm
+sudo apt-get install -y php7.1-recode
+sudo apt-get install -y php7.1-odbc
+sudo apt-get install -y php7.1-gmp
+sudo apt-get install -y php7.1-common
+sudo apt-get install -y php7.1-pgsql
+sudo apt-get install -y php7.1-bcmath
+sudo apt-get install -y php7.1-snmp
+sudo apt-get install -y php7.1-soap
+sudo apt-get install -y php7.1-mcrypt
+sudo apt-get install -y php7.1-gd
+sudo apt-get install -y php7.1-enchant
+sudo apt-get install -y libapache2-mod-php7.1
+sudo apt-get install -y libphp7.1-embed
